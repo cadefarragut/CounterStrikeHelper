a397c55fb1e4ad344e9ca3756ab0fca753be3c4b
cade.farragut <cade_farragut@yahoo.com>
2026-01-05 23:40:33 -0600

    Webhook working with URL

--- main.cpp
+++ main.cpp
@@ -1,15 +1,38 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#define CPPHTTPLIB_OPENSSL_SUPPORT
 #include "include/httplib.h"
 
 using namespace std;
+using namespace httplib;
+
+string getenv_string(const char* key){
+  const char* env = getenv(key);
+  if(!env){
+    cout << "Incorrect key name: " << env << endl;
+    exit(1);
+  }
+  return string(env);
+}
 
 int main(){
-  const char* web_hook = getenv("DISCORD_WEBHOOK_URL");
-  const char* leetify_api = getenv("LEETIFY_API_KEY");
+  string web_hook = getenv_string("DISCORD_WEBHOOK_URL");
+  string leetify_api = getenv_string("LEETIFY_API_KEY");
 
-  cout << "Hello World" << endl;
+  Client cli("https://discord.com");
+  auto res = cli.Post(
+      web_hook,
+      R"({"content":"Test"})",
+      "application/json"
+      );
+
+  if(res){
+    cout << "Status: " << res->status << endl; 
+  } else {
+    cout << "error sending POST" << endl;
+  }
+
   
   return 0;
 }
